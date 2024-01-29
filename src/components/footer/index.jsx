358b54e0cb9f7c67ba4ac9343abfffc1ca506f54
kakagenz <kakagenz86@gmail.com>
2024-01-29 21:26:04 +0800

    fix and change project

--- src/components/footer/index.jsx
+++ src/components/footer/index.jsx
@@ -1,26 +1,25 @@
-import React from 'react'
 import './style.css'
 import img from '../../assets/images/me.png'
 
 const Footer = () => {
     return (
-        <div class="footer">
+        <div className="footer">
             <div className="container">
-                <div class="row footer-wrapper">
-                    <div class="col-md-6 col-sm-12 col-xl-3 mt-5 mb-5">
-                        <p class="footer-text-wrapper">Jalan Duyung Asri Barat Gang 2 Gilimanuk, Bali, Kec.Melaya, Kab.Jembrana</p>
-                        <p class="footer-text-wrapper">kakagenz86@gmail.com</p>
-                        <p class="footer-text-wrapper">085-157-718-328</p>
+                <div className="row footer-wrapper">
+                    <div className="col-md-6 col-sm-12 col-xl-3 mt-5 mb-5">
+                        <p className="footer-text-wrapper">Jalan Duyung Asri Barat Gang 2 Gilimanuk, Bali, Kec.Melaya, Kab.Jembrana</p>
+                        <p className="footer-text-wrapper">kakagenz86@gmail.com</p>
+                        <p className="footer-text-wrapper">085-157-718-328</p>
                     </div>
-                    <div class="col-md-6 col-sm-12 col-xl-3 mt-5 mb-5">
+                    <div className="col-md-6 col-sm-12 col-xl-3 mt-5 mb-5">
                         <h4 className='fw-bold'>About</h4>
-                        <a class="footer-text-wrapper-2" href="#beranda" >Beranda</a>
-                        <a class="footer-text-wrapper-2" href="#tentangsaya">Tentang Saya</a>
-                        <a class="footer-text-wrapper-2" href="#project" >Project</a>
-                        <a class="footer-text-wrapper-2" href="#contact" >Contact</a>
+                        <a className="footer-text-wrapper-2" href="#beranda" >Beranda</a>
+                        <a className="footer-text-wrapper-2" href="#tentangsaya">Tentang Saya</a>
+                        <a className="footer-text-wrapper-2" href="#project" >Project</a>
+                        <a className="footer-text-wrapper-2" href="#contact" >Contact</a>
                     </div>
-                    <div class="col-md-6 col-sm-12 col-xl-3 mt-5 mb-5">
-                        <p class="footer-text-wrapper">Copyright Kaka Hardiansyah 2023</p>
+                    <div className="col-md-6 col-sm-12 col-xl-3 mt-5 mb-5">
+                        <p className="footer-text-wrapper">Copyright Kaka Hardiansyah 2023</p>
                         <img src={img} className='footer-logo' alt="binar-logo" />
                     </div>
                 </div>
--- src/pages/homepages/index.jsx
+++ src/pages/homepages/index.jsx
@@ -4,70 +4,62 @@ import AboutMe from '../../components/about';
 import Project from '../../components/project';
 import Contact from '../../components/Contact';
 import Footer from '../../components/footer';
-import img1 from '../../assets/images/img1.png'
-import img2 from '../../assets/images/img2.png'
-import img3 from '../../assets/images/img3.png'
-import img4 from '../../assets/images/img4.png'
-import img5 from '../../assets/images/img5.png'
-import img6 from '../../assets/images/img6.png'
-import img7 from '../../assets/images/img7.png'
-import img8 from '../../assets/images/img8.png'
-import img9 from '../../assets/images/img9.png'
+
 
 const HomePages = () => {
     const projectMe = [{
-        image: img1,
+        image: 'https://placeholder.com/200x200',
         description: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Repellendus, magnam a! Iste laboriosam fugiat itaque mollitia odit, necessitatibus facilis ducimus.',
         alt: 'web wedding image',
-        link: 'https://wedding-web-five.vercel.app/'
+        link: '#'
     },
     {
-        image: img2,
+        image: 'https://placeholder.com/200x200',
         description: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Repellendus, magnam a! Iste laboriosam fugiat itaque mollitia odit, necessitatibus facilis ducimus.',
         alt: 'company profile Utama Medical',
-        link: 'https://utama-medical.vercel.app/'
+        link: '#'
     },
     {
-        image: img3,
+        image: 'https://placeholder.com/200x200',
         description: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Repellendus, magnam a! Iste laboriosam fugiat itaque mollitia odit, necessitatibus facilis ducimus.',
         alt: 'olshop image',
-        link: 'https://onlineshoes.vercel.app/'
+        link: '#'
     },
     {
-        image: img4,
+        image: 'https://placeholder.com/200x200',
         description: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Repellendus, magnam a! Iste laboriosam fugiat itaque mollitia odit, necessitatibus facilis ducimus.',
         alt: 'mini portfolio image',
-        link: 'https://miniport.vercel.app/'
+        link: '#'
     },
     {
-        image: img5,
+        image: 'https://placeholder.com/200x200',
         description: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Repellendus, magnam a! Iste laboriosam fugiat itaque mollitia odit, necessitatibus facilis ducimus.',
         alt: 'car rental image',
-        link: 'https://car-rental-gilt-eight.vercel.app/'
+        link: '#'
     },
     {
-        image: img6,
+        image: 'https://placeholder.com/200x200',
         description: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Repellendus, magnam a! Iste laboriosam fugiat itaque mollitia odit, necessitatibus facilis ducimus.',
         alt: 'shop affiliate image',
-        link: 'https://kandaaaff.vercel.app/'
+        link: '#'
     },
     {
-        image: img7,
+        image: 'https://placeholder.com/200x200',
         description: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Repellendus, magnam a! Iste laboriosam fugiat itaque mollitia odit, necessitatibus facilis ducimus.',
         alt: 'box-model image',
-        link: 'https://boxmodel-cyan.vercel.app/'
+        link: '#'
     },
     {
-        image: img8,
+        image: 'https://placeholder.com/200x200',
         description: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Repellendus, magnam a! Iste laboriosam fugiat itaque mollitia odit, necessitatibus facilis ducimus.',
         alt: 'react from binar image',
-        link: 'https://cp6-react5-kakagenz86s-projects.vercel.app/'
+        link: '#'
     },
     {
-        image: img9,
+        image: 'https://placeholder.com/200x200',
         description: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Repellendus, magnam a! Iste laboriosam fugiat itaque mollitia odit, necessitatibus facilis ducimus.',
         alt: 'web film image',
-        link: 'https://genz-film.vercel.app/'
+        link: '#'
     }]
 
     return ( 
